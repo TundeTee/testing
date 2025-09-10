e63cb5cf9210f3fbb90ba26be889ff40e4699375
Tunde <olaseni2019@gmail.com>
2025-09-10 22:45:45 +0100

    done
    done

--- home.js
+++ home.js
@@ -1,11 +1,12 @@
  var user = localStorage.getItem("user");
     if(user){
         var parsedUser = JSON.parse(user);
-        document.getElementById("welcomemessage").innerHTML = `Hello ${parsedUser.name} <br> Welcome to our site!`;
+        document.getElementById("welcomemessage").innerHTML = `Hello ${parsedUser.name} <br> Welcome to our site! <br><br>`;
         document.getElementById("logoutBtn").classList.remove("hidden");
+        document.getElementById("logo").innerHTML = `<i class="ri-user-follow-line"></i>`;
     }
     else{
-        document.getElementById("welcomemessage").innerHTML = `Hello, please <br> <a href="login.html"> login </a>`;
+        document.getElementById("welcomemessage").innerHTML = `Hello, please <br> <a href="login.html" > login </a>`;
     }
     document.getElementById("logoutBtn").addEventListener("click", function(){
         localStorage.removeItem("user");
